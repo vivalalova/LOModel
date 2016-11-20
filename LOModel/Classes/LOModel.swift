66097bb1a5081733f9a19ee07d7ae54c69b724f8
vivalalova <vivalalova0@gmail.com>
2016-11-20 15:40:04 +0800

    updated

--- LOModel/Classes/LOModel.swift
+++ LOModel/Classes/LOModel.swift
@@ -8,36 +8,45 @@
 
 import UIKit
 
-open class LOModel:NSObject {
-    
-    public init(dict:[String:Any]?) {
+open class LOModel: NSObject {
+
+    public init(dict: [String: Any]?) {
         super.init()
-        
+
         guard dict != nil else {
             return
         }
-        
+
         self.setValuesForKeys(dict!)
     }
 
-    open func toDictionary() -> [String:Any] {
-        let propertyNames = Mirror(reflecting: self).children.flatMap {$0.label}
+//    open func array<T>(dicts:[[String:Any]]) -> [T] {
+//        var results = [T]()
+//        for dict in dicts {
+//            var xx = T()
+//        }
+//
+//        return results
+//    }
+
+    open func toDictionary() -> [String: Any] {
+        let propertyNames = Mirror(reflecting: self).children.flatMap { $0.label }
         return self.dictionaryWithValues(forKeys: propertyNames)
     }
-    
-    open func mapping() -> [String:String] {
-        return [String:String]()
+
+    open func mapping() -> [String: String] {
+        return [String: String]()
     }
-    
+
     open override func setValue(_ value: Any?, forKey key: String) {
         super.setValue(value, forKey: key)
     }
-    
+
     open override func setValue(_ value: Any?, forUndefinedKey key: String) {
         guard let mappedKey = self.mapping()[key] else {
             return
         }
-        
+
         self.setValue(value, forKey: mappedKey)
     }
 }
